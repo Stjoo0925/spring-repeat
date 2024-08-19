1532396bf969656f6b60260bb147f408b001e8de
Stjoo0925 <stjoo0925@gmail.com>
2024-08-19 22:15:48 +0900

    fix : UserEntity update

--- src/main/java/com/test/springrepeat/user/entity/UserEntity.java
+++ src/main/java/com/test/springrepeat/user/entity/UserEntity.java
@@ -1,7 +1,9 @@
 package com.test.springrepeat.user.entity;
 
-
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -9,102 +11,113 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private final Integer id;
 
     @Column(name = "user_name")
-    private String userName;
+    private final String userName;
 
     @Column(name = "user_age")
-    private Integer userAge;
+    private final Integer userAge;
 
     @Column(name = "user_ageaddress_post")
-    private String addressPost;
+    private final String addressPost;
 
     @Column(name = "user_address_default")
-    private String addressDefault;
+    private final String addressDefault;
 
     @Column(name = "user_address_detail")
-    private String addressDetail;
+    private final String addressDetail;
 
     @Column(name = "user_create_at")
-    private LocalDateTime userCreateAt;
+    @CreationTimestamp
+    private final LocalDateTime userCreateAt;
 
     @Column(name = "user_update_at")
-    private LocalDateTime userUpdateAt;
+    @UpdateTimestamp
+    private final LocalDateTime userUpdateAt;
 
-    @Column(name = "user_delete_at")
-    private LocalDateTime userDeleteAt;
-
-    private UserEntity(Integer id, String userName, Integer userAge, String addressPost, String addressDefault, String addressDetail, LocalDateTime userCreateAt, LocalDateTime userUpdateAt, LocalDateTime userDeleteAt) {
-        this.id = id;
-        this.userName = userName;
-        this.userAge = userAge;
-        this.addressPost = addressPost;
-        this.addressDefault = addressDefault;
-        this.addressDetail = addressDetail;
-        this.userCreateAt = userCreateAt;
-        this.userUpdateAt = userUpdateAt;
-        this.userDeleteAt = userDeleteAt;
+    protected UserEntity() {
+        this.id = null;
+        this.userName = null;
+        this.userAge = null;
+        this.addressPost = null;
+        this.addressDefault = null;
+        this.addressDetail = null;
+        this.userCreateAt = null;
+        this.userUpdateAt = null;
     }
 
-    public UserEntity() {
+    private UserEntity(Builder builder) {
+        this.id = builder.id;
+        this.userName = builder.userName;
+        this.userAge = builder.userAge;
+        this.addressPost = builder.addressPost;
+        this.addressDefault = builder.addressDefault;
+        this.addressDetail = builder.addressDetail;
+        this.userCreateAt = builder.userCreateAt;
+        this.userUpdateAt = builder.userUpdateAt;
     }
 
-    public static UserEntity builder(){
-        return new UserEntity();
+    public static Builder builder() {
+        return new Builder();
     }
 
+    public static class Builder {
+        private Integer id;
+        private String userName;
+        private Integer userAge;
+        private String addressPost;
+        private String addressDefault;
+        private String addressDetail;
+        private LocalDateTime userCreateAt;
+        private LocalDateTime userUpdateAt;
 
-    public UserEntity id(Integer id) {
-        this.id = id;
-        return this;
-    }
-
-    public UserEntity userName(String userName) {
-        this.userName = userName;
-        return  this;
-    }
-
-    public UserEntity userAge(Integer userAge) {
-        if(userAge < 20){
-
-            return null;
+        public Builder id(Integer id) {
+            this.id = id;
+            return this;
         }
-        this.userAge = userAge;
-        return this;
-    }
 
-    public UserEntity addressPost(String addressPost) {
-        this.addressPost = addressPost;
-        return this;
-    }
+        public Builder userName(String userName) {
+            this.userName = userName;
+            return this;
+        }
 
-    public UserEntity addressDefault(String addressDefault) {
-        this.addressDefault = addressDefault;
-        return this;
-    }
+        public Builder userAge(Integer userAge) {
+            if (userAge < 20) {
+                throw new IllegalArgumentException("회원의 나이는 20살 이상이여야 합니다.");
+            }
+            this.userAge = userAge;
+            return this;
+        }
 
-    public UserEntity addressDetail(String addressDetail) {
-        this.addressDetail = addressDetail;
-        return this;
-    }
+        public Builder addressPost(String addressPost) {
+            this.addressPost = addressPost;
+            return this;
+        }
 
-    public UserEntity userCreateAt(LocalDateTime userCreateAt) {
-        this.userCreateAt = userCreateAt;
-        return this;
-    }
+        public Builder addressDefault(String addressDefault) {
+            this.addressDefault = addressDefault;
+            return this;
+        }
 
-    public UserEntity userUpdateAt(LocalDateTime userUpdateAt) {
-        this.userUpdateAt = userUpdateAt;
-        return this;
-    }
+        public Builder addressDetail(String addressDetail) {
+            this.addressDetail = addressDetail;
+            return this;
+        }
 
-    public UserEntity userDeleteAt(LocalDateTime userDeleteAt) {
-        this.userDeleteAt = userDeleteAt;
-        return this;
-    }
-    public UserEntity build() {
-        return new UserEntity(id,userName,userAge,addressPost,addressDefault,addressDetail,userCreateAt,userUpdateAt,userDeleteAt);
+        public Builder userCreateAt(LocalDateTime userCreateAt) {
+            this.userCreateAt = userCreateAt;
+            return this;
+        }
+
+        public Builder userUpdateAt(LocalDateTime userUpdateAt) {
+            this.userUpdateAt = userUpdateAt;
+            return this;
+        }
+
+        public UserEntity build() {
+            return new UserEntity(this);
+        }
     }
 
     @Override
@@ -118,7 +131,6 @@ public class UserEntity {
                 ", addressDetail='" + addressDetail + '\'' +
                 ", userCreateAt=" + userCreateAt +
                 ", userUpdateAt=" + userUpdateAt +
-                ", userDeleteAt=" + userDeleteAt +
                 '}';
     }
 }
