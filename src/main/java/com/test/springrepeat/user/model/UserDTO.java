3cfd7b249ba75cf791974255fdf22f54e497e674
Quokka <wotjr1142@gmail.com>
2024-08-19 17:36:16 +0900

    fix : Users

---
+++ src/main/java/com/test/springrepeat/user/model/UserDTO.java
@@ -0,0 +1,16 @@
+package com.test.springrepeat.user.model;
+
+public class UserDTO {
+
+
+
+//    private Integer id;
+//    private String userName;
+//    private Integer userAge;
+//    private String addressPost;
+//    private String addressDefault;
+//    private String addressDetail;
+//    private LocalDateTime userCreateAt;
+//    private LocalDateTime userUpdateAt;
+//    private LocalDateTime userDeleteAt;
+}
