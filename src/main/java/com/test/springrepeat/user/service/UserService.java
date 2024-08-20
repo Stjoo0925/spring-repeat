5c2acfca6193751dddaa6fb7d44e65f8ec4049d0
hwangjeonghan <hjeonghan0@gmail.com>
2024-08-20 11:55:19 +0900

    feat : update controller,service

--- src/main/java/com/test/springrepeat/user/service/UserService.java
+++ src/main/java/com/test/springrepeat/user/service/UserService.java
@@ -2,13 +2,13 @@ package com.test.springrepeat.user.service;
 
 
 import com.test.springrepeat.user.entity.UserEntity;
-import com.test.springrepeat.user.model.UserDTO;
+
 import com.test.springrepeat.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +27,15 @@ public class UserService {
     @Transactional
     public List<UserEntity> findAllUser() {
         return userRepository.findAll();
+    }
+
+    //상세조회
+    @Transactional
+    public Optional<UserEntity> findUserById(Integer id) {
+        if (id == null || id <= 0) {
+            throw new IllegalArgumentException("ID는 0보다 커야 합니다.");
+        }
+        return userRepository.findById(id);
     }
 }
 
