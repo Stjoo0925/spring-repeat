74f6a09a82cacc617ab331d937366401c4a6c792
Quokka <wotjr1142@gmail.com>
2024-08-20 10:48:20 +0900

    feat: order all view

--- src/main/java/com/test/springrepeat/order/dto/OrderDTO.java
+++ src/main/java/com/test/springrepeat/order/dto/OrderDTO.java
@@ -9,4 +9,87 @@ public class OrderDTO {
     private String orderUpdateAt;
     private String orderDeleteAt;
 
+    public OrderDTO() {
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public Integer getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(Integer orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public Integer getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(Integer orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
+
+    public String getOrderCreateAt() {
+        return orderCreateAt;
+    }
+
+    public void setOrderCreateAt(String orderCreateAt) {
+        this.orderCreateAt = orderCreateAt;
+    }
+
+    public String getOrderUpdateAt() {
+        return orderUpdateAt;
+    }
+
+    public void setOrderUpdateAt(String orderUpdateAt) {
+        this.orderUpdateAt = orderUpdateAt;
+    }
+
+    public String getOrderDeleteAt() {
+        return orderDeleteAt;
+    }
+
+    public void setOrderDeleteAt(String orderDeleteAt) {
+        this.orderDeleteAt = orderDeleteAt;
+    }
+
+    public OrderDTO(Integer orderId, String orderName, Integer orderPrice, Integer orderQuantity, String orderCreateAt, String orderUpdateAt, String orderDeleteAt) {
+
+        this.orderId = orderId;
+        this.orderName = orderName;
+        this.orderPrice = orderPrice;
+        this.orderQuantity = orderQuantity;
+        this.orderCreateAt = orderCreateAt;
+        this.orderUpdateAt = orderUpdateAt;
+        this.orderDeleteAt = orderDeleteAt;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "orderId=" + orderId +
+                ", orderName='" + orderName + '\'' +
+                ", orderPrice=" + orderPrice +
+                ", orderQuantity=" + orderQuantity +
+                ", orderCreateAt='" + orderCreateAt + '\'' +
+                ", orderUpdateAt='" + orderUpdateAt + '\'' +
+                ", orderDeleteAt='" + orderDeleteAt + '\'' +
+                '}';
+    }
+
 }
