095fd36cacbf0c1ad8372bfc0b8e4411316c6216
hwangjeonghan <hjeonghan0@gmail.com>
2024-08-20 10:33:41 +0900

    feat : update entity

--- src/main/java/com/test/springrepeat/user/entity/UserEntity.java
+++ src/main/java/com/test/springrepeat/user/entity/UserEntity.java
@@ -3,6 +3,8 @@ package com.test.springrepeat.user.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Entity
 public class UserEntity {
@@ -20,13 +22,13 @@ public class UserEntity {
     @Column(name = "user_ageaddress_post")
     private String addressPost;
 
-    @Column(name = "user_address_default")
+    @Column(name = "user_address_default", nullable = false)
     private String addressDefault;
 
-    @Column(name = "user_address_detail")
+    @Column(name = "user_address_detail", nullable = false )
     private String addressDetail;
 
-    @Column(name = "user_create_at")
+    @Column(name = "user_create_at",  nullable = false)
     private LocalDateTime userCreateAt;
 
     @Column(name = "user_update_at")
@@ -60,31 +62,54 @@ public class UserEntity {
         return this;
     }
 
+
     public UserEntity userName(String userName) {
-        this.userName = userName;
-        return  this;
+        Pattern p = Pattern.compile("^[가-힣]{3}$");
+
+        Matcher m = p.matcher(userName);
+        if(m.matches()){
+            this.userName = userName;
+            return this;
+        }else {
+           throw new IllegalArgumentException("[경고]성명은 공백이 없는 한글로만 이루어져야 하며 3글자로만 되어져야 합니다.");
+        }
     }
 
     public UserEntity userAge(Integer userAge) {
-        if(userAge < 20){
-
-            return null;
+        if(userAge >= 20){
+            this.userAge = userAge;
+            return this;
+        }else {
+            throw new IllegalArgumentException("20살 보다 작습니다.");
         }
-        this.userAge = userAge;
-        return this;
     }
 
     public UserEntity addressPost(String addressPost) {
-        this.addressPost = addressPost;
+        Pattern p = Pattern.compile("^[0-4]*$");
+
+        Matcher m = p.matcher(addressPost);
+
+        if(m.matches()){
+            this.addressPost = addressPost;
+
+        }else {
+            throw new IllegalArgumentException("[경고]숫자는 5자리 숫자 까지만 가능합니다.");
+        }
         return this;
     }
 
     public UserEntity addressDefault(String addressDefault) {
+        if(addressDefault == null){
+            throw new IllegalArgumentException("기본주소는 생략 할 수 없습니다.");
+        }
         this.addressDefault = addressDefault;
         return this;
     }
 
     public UserEntity addressDetail(String addressDetail) {
+        if(addressDetail==null){
+            throw new IllegalArgumentException("기본 주소는 삭제 할 수 없습니다.");
+        }
         this.addressDetail = addressDetail;
         return this;
     }
@@ -103,22 +128,7 @@ public class UserEntity {
         this.userDeleteAt = userDeleteAt;
         return this;
     }
-    public UserEntity build() {
-        return new UserEntity(id,userName,userAge,addressPost,addressDefault,addressDetail,userCreateAt,userUpdateAt,userDeleteAt);
-    }
 
-    @Override
-    public String toString() {
-        return "UserEntity{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", userAge=" + userAge +
-                ", addressPost='" + addressPost + '\'' +
-                ", addressDefault='" + addressDefault + '\'' +
-                ", addressDetail='" + addressDetail + '\'' +
-                ", userCreateAt=" + userCreateAt +
-                ", userUpdateAt=" + userUpdateAt +
-                ", userDeleteAt=" + userDeleteAt +
-                '}';
-    }
+
 }
+
